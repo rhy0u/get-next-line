e093d894698b0c0c937bc4da577f750e7a92e51d
Jeason Centaure <jcentaur@student.42.fr>
2016-12-24 11:49:30 +0100

    lol

--- get_next_line.c
+++ get_next_line.c
@@ -1,66 +1,42 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jcentaur <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/16 16:53:20 by jcentaur          #+#    #+#             */
-/*   Updated: 2016/12/16 17:15:48 by jcentaur         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
-int		ft_is_next(char *s)
+t_save	*search_fd(int fd, t_save **save)
 {
-	int		i;
-
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == '\n')
-			return (i);
-		i++;
-	}
-	return (-1);
+	if (!*save)
+		if(!(*save = (t_save *)malloc(sizeof(t_save))))
+			return (NULL);
+		return (*save);
+	if ((*save)->fd == fd)
+		return (*save);
+	return (search_fd(fd, &(*save)->next));
 }
+
+int		get_line(char **line)
+{
+	int i;
+
+
 
 int		get_next_line(int fd, char **line)
 {
-	int				next;
 	int				ret;
-	static char		*tmp = "\0";
 	char			buf[BUFF_SIZE + 1];
+	t_save			*to_read;
+	static t_save	*save = NULL;
 
-	*line = "\0";
-	next = -1;
 	ret = 1;
-	*line = ft_strjoin(tmp, *line);
-	if (ft_is_next(*line) != -1)
-	{
-		*line = ft_strsub(*line, 0, ft_is_next(*line));
-		tmp = ft_strsub(tmp, ft_is_next(tmp) + 1,
-			ft_strlen(tmp) - ft_is_next(tmp) - 1);
-		if (!**line)
-			return(-1);
-		if (!*tmp)
-			return (0);
-		return (1);
-	}
-	while (next == -1 && ret != 0 && ret != -1)
+
+	if (fd < 0)
+		return (-1);
+	to_read = search_fd(fd, &save);
+	while (ret != -1 && ret)
 	{
 		ret = read(fd, buf, BUFF_SIZE);
-		buf[ret] = '\0';
+		to_read->buf[ret] = '\0';
 		*line = ft_strjoin(*line, buf);
-		next = ft_is_next(buf);
 	}
-	tmp = ft_strsub(*line, ft_is_next(*line) + 1,
-		ft_strlen(*line) - ft_is_next(*line) - 1);
-	*line = ft_strsub(*line, 0, ft_is_next(*line));
-	if (!ret)
-		return (0);
-	if (!line)
-		return (-1);
-	return (1);
+
+	
+
+
 }
--- get_next_line.h
+++ get_next_line.h
@@ -20,8 +20,15 @@
 # include <stdio.h>
 # include <unistd.h>
 # define BUFF_SIZE 1
-int   get_next_line(int fd, char **line);
-char	*ft_strjoin(const char *s1, const char *s2);
-char	*ft_strsub(const char *s, int start, size_t size);
-size_t	ft_strlen(const char *s);
+
+typedef struct	s_save
+{
+	int			fd;
+	char		buf[BUFF_SIZE + 1];
+	s_save		*next;
+}				t_save;
+
+char			*ft_strjoin(const char *s1, const char *s2);
+char			*ft_strsub(const char *s, int start, size_t size);
+size_t			ft_strlen(const char *s);
 #endif
