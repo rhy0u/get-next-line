53cd25091eb2b4be8a94cc96ae7b8dbd4aba26f1
Jeason Centaure <jcentaur@student.42.fr>
2017-03-25 17:45:47 +0100

    0 leaks

--- get_next_line.c
+++ get_next_line.c
@@ -19,7 +19,7 @@ t_save	*search_fd(int fd, t_save **save)
 		if (!(*save = (t_save*)malloc(sizeof(t_save))))
 			return (NULL);
 		(*save)->fd = fd;
-		(*save)->buf = "";
+		(*save)->buf = NULL;
 		(*save)->next = NULL;
 	}
 	if ((*save)->fd == fd)
@@ -27,31 +27,36 @@ t_save	*search_fd(int fd, t_save **save)
 	return (search_fd(fd, &((*save)->next)));
 }
 
-int		get_line(char *line)
+int		get_line(char *s)
 {
 	int i;
 
-	if (!line)
+	if (!s)
 		return (-1);
 	i = 0;
-	while (line[i] && line[i] != '\n' && line[i] != '\r')
+	while (s[i] && s[i] != '\n' && s[i] != '\r')
 		i++;
-	return (line[i]) ? i : -1;
+	return (s[i]) ? i : -1;
 }
 
-void	ft_cheat(char **line, t_save *tmp)
+void	ft_cheat(char **line, t_save *tmp, char **test)
 {
 	int		r;
 
-	r = get_line(*line);
-	if (get_line(*line) != -1)
+	r = get_line(*test);
+	if (r != -1)
 	{
-		tmp->buf = ft_strsub(*line, get_line(*line) + 1,
-			ft_strlen(*line) - get_line(*line) - 1);
-		*line = ft_strsub(*line, 0, get_line(*line));
+		tmp->buf = ft_strsub(*test, r + 1,
+			ft_strlen(*test) - (r + 1));
+		*line = ft_strsub(*test, 0, r);
+		ft_strdel(test);
 	}
 	else
-		tmp->buf = "";
+	{
+		*line = ft_strdup(*test);
+		ft_strdel(test);
+		tmp->buf = NULL;
+	}
 }
 
 int		get_next_line(int fd, char **line)
@@ -59,21 +64,27 @@ int		get_next_line(int fd, char **line)
 	int				ret;
 	char			buf[BUFF_SIZE + 1];
 	t_save			*tmp;
+	char			*test;
 	static t_save	*save = NULL;
 
 	if (!line || fd < 0)
 		return (-1);
 	tmp = search_fd(fd, &save);
-	*line = ft_strdup(tmp->buf);
+	test = NULL;
+	if (tmp->buf)
+	{
+		test = ft_strdup(tmp->buf);
+		free(tmp->buf);
+	}
 	ret = 1;
-	while (ret && get_line(*line) == -1)
+	while (ret && get_line(test) == -1)
 	{
 		ret = read(fd, buf, BUFF_SIZE);
 		if (ret == -1)
 			return (-1);
 		buf[ret] = '\0';
-		*line = ft_strjoinf(*line, buf);
+		test = ft_strjoinf(test, buf);
 	}
-	ft_cheat(line, tmp);
-	return ((!ret && !**line && !tmp->buf[0]) ? 0 : 1);
+	ft_cheat(line, tmp, &test);
+	return ((!ret && !**line && !tmp->buf) ? 0 : 1);
 }
